ce7d58d3bf796a17180d766c9566524a7848abd6
Daniel Schmidt <dschmidt@weluse.de>
2013-10-30 22:33:36 +0100

    improved save, so it works (except it's mocked)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,27 +3,46 @@
 var app = angular.module('questionmasterAngularApp');
 
 app.controller('MainCtrl', function ($scope) {
-    $scope.save = function(newQuestion){
-      console.log("Yeah, well i didn't saved that one exactly");
-      $scope.newQuestion = {}
+  var defaultQuestion = function(){
+    return {
+      question: "...",
+      answer: "..."
     }
+  }
 
-    $scope.questions = [
-      {
-        'question': 'Where are my keys?',
-        'answer': 'Behind you'
-      },{
-        'question': 'How much is the fish?',
-        'answer': '4.20 &euro;'
-      },{
-        'question': 'Where is Google',
-        'answer': '<a href="http://www.google.de">Here</a>'
-      }
-    ];
-  });
+  $scope.newQuestion = defaultQuestion();
+
+  $scope.save = function(newQuestion){
+    // Don't add if it doesnt differ
+    if($scope.newQuestion.question !== defaultQuestion().question){
+      $scope.questions.push($scope.newQuestion);
+      $scope.newQuestion = defaultQuestion();
+    }
+  }
+
+  $scope.questions = [
+    {
+      'question': 'Where are my keys?',
+      'answer': 'Behind you'
+    },{
+      'question': 'How much is the fish?',
+      'answer': '4.20 &euro;'
+    },{
+      'question': 'Where is Google',
+      'answer': '<a href="http://www.google.de">Here</a>'
+    }
+  ];
+
+});
 
 app.directive('question', function(){
   return {
     templateUrl: 'views/directives/question.html'
   }
+});
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
 });
