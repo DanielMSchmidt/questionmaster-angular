02fa8a18cc0e95aaabf5b4558a1fa3471167d15a
Daniel Schmidt <dschmidt@weluse.de>
2013-10-30 21:50:37 +0100

    mocked questions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,16 @@
 
 angular.module('questionmasterAngularApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.questions = [
+      {
+        'question': 'Where are my keys?',
+        'answer': 'Behind you'
+      },{
+        'question': 'How much is the fish?',
+        'answer': '4.20 &euro;'
+      },{
+        'question': 'Where is Google',
+        'answer': '<a href="http://www.google.de">Here</a>'
+      }
     ];
   });
